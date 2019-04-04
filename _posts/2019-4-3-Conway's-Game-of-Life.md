785eb5d7790a0fe8773f6fd2582708b59780c6f3
yliapis <yliapis44@gmail.com>
2019-04-04 01:14:05 -0500

    some refactoring (WIP, has bugs)

--- _posts/2019-4-3-Conway's-Game-of-Life.md
+++ _posts/2019-4-3-Conway's-Game-of-Life.md
@@ -10,7 +10,6 @@ category: posts
     <div id="stage" padding="0 px"></div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjs/3.3.0/math.min.js"></script>
     <script src="/assets/js_libs/two.js"></script>
-    <!-- <script src="/assets/js_apps/Conway's-Game-of-Life/app.js"></script> -->
     <script src="app.js"></script>
   </body>
 </html>
--- assets/js_apps/Conway's-Game-of-Life/app.js
+++ assets/js_apps/Conway's-Game-of-Life/app.js
@@ -3,17 +3,16 @@
 var elem = document.getElementById("stage");
 var params = { width: 500, height: 500 };
 var two = new Two(params).appendTo(elem);
+var margin = 0;
 
 let ON = "black";
 let OFF = "white";
 
 // define grid parameters
 var ngrid = { width: 50, height: 50 };
-var margin = 0;
-var step = 10;
-var size = step - 2*margin;
 
-var p = .5;
+// ratio of dead/alive cells at startup
+var P = 0.5;
 
 // create array for conway's game of life
 function matrix(m, n) {
@@ -24,91 +23,111 @@ function matrix(m, n) {
   return grid;
 }
 
-grid = matrix(ngrid.height, ngrid.width);
+// Game object constructor
+function GameofLife(ngrid, frame_step) {
 
-function initializeGrid(grid) {
-  for (var m = 0; m < ngrid.height; m++) {
-    for (var n = 0; n < ngrid.width; n++) {
-      grid[m][n] = (Math.random() > p);
+  var step = ngrid.width/params.width;
+  var frame_step = frame_step | 2;
+  var ngrid = ngrid;
+  
+  function initializeGrid(grid) {
+    for (var m = 0; m < ngrid.height; m++) {
+      for (var n = 0; n < ngrid.width; n++) {
+        grid[m][n] = (Math.random() > P);
+      }
     }
+    return grid;
   }
-}
 
-function numNeighbors(grid, m, n) {
-  var sum = 0;
-  for (var i = -1; i <= 1; i++) {
-    for (var j = -1; j <= 1; j++) {
-      var mm = m + i;
-      var nn = n + j;
-      if ((0 <= mm) && (mm < grid.length) &&
-          (0 <= nn) && (nn < grid[0].length) &&
-          !((m == mm) && (n == nn)) && (grid[mm][nn]))
-        sum++;
+  function numNeighbors(grid, m, n) {
+    var sum = 0;
+    for (var i = -1; i <= 1; i++) {
+      for (var j = -1; j <= 1; j++) {
+        var mm = m + i;
+        var nn = n + j;
+        if ((0 <= mm) && (mm < grid.length) &&
+            (0 <= nn) && (nn < grid[0].length) &&
+            !((m == mm) && (n == nn)) && (grid[mm][nn]))
+          sum++;
+      }
     }
+    return sum;
   }
-  return sum;
-}
 
-function updateConway(alive, neighbors) {
-  if (alive) {
-    if ((2 <= neighbors) && (neighbors <= 3))
-      return true;
-    else
-      return false;
-  }
-  else {
-    if (neighbors == 3)
-      return true;
-    else
-      return false;
-  }
-}
-
-function stepGame(grid) {
-  var next = matrix(ngrid.height, ngrid.width);
-  for (var m = 0; m < ngrid.height; m++) {
-    for (var n = 0; n < ngrid.width; n++) {
-      next[m][n] = updateConway(grid[m][n], numNeighbors(grid, m, n));
+  function updateCell(alive, neighbors) {
+    if (alive) {
+      if ((2 <= neighbors) && (neighbors <= 3))
+        return true;
+      else
+        return false;
     }
-  }
-  return next;
-}
-
-// create square elements
-var squares = new Array(ngrid.height);
-for (var m = 0; m < ngrid.height; m++) {
-    squares[m] = Array(ngrid.width)
-    for (var n = 0; n < ngrid.width; n++) {
-      var corner = { x: m * step + margin, y: n * step + margin};
-      rect = two.makeRectangle(corner.x + step/2, corner.y + step/2,
-                               size, size);
-      rect.fill = OFF;
-      rect.linewidth = 0;
-      squares[m][n] = rect
+    else {
+      if (neighbors == 3)
+        return true;
+      else
+        return false;
     }
   }
 
-function render(grid) {
-  for (var m = 0; m < ngrid.height; m++) {
-    for (var n = 0; n < ngrid.width; n++) {
-      rect = squares[m][n];
-      if (grid[m][n]) rect.fill = ON;
-      else rect.fill = OFF;
+  function stepGame(grid) {
+    var next = matrix(ngrid.height, ngrid.width);
+    for (var m = 0; m < ngrid.height; m++) {
+      for (var n = 0; n < ngrid.width; n++) {
+        next[m][n] = updateCell(grid[m][n],
+                                numNeighbors(grid, m, n));
+      }
     }
-  } 
+    return next;
+  }
+
+  function constructElementGrid(grid) {
+    var element_grid = Array(ngrid.height);
+    for (var m = 0; m < ngrid.height; m++) {
+      element_grid[m] = Array(ngrid.width)
+      for (var n = 0; n < ngrid.width; n++) {
+        var corner = { x: m * step + margin, y: n * step + margin};
+        rect = two.makeRectangle(corner.x + step/2, corner.y + step/2,
+                                 step, step);
+        rect.fill = OFF;
+        rect.linewidth = 0;
+        element_grid[m][n] = rect
+      }
+    }
+    return element_grid;
+  }
+
+  this.grid = initializeGrid(matrix(ngrid.height, ngrid.width));
+  this.element_grid = constructElementGrid(this.grid);
+
+  function transfer() {
+    for (var m = 0; m < ngrid.height; m++) {
+      for (var n = 0; n < ngrid.width; n++) {
+        rect = this.element_grid[m][n];
+        if (this.grid[m][n]) rect.fill = ON;
+        else rect.fill = OFF;
+      }
+    } 
+  }
+
+  two.bind("update", function(frameCount) {
+    if (!(frameCount % frame_step)) {
+      this.grid = stepGame(this.grid);
+      transfer();
+    }
+  });
+
+  console.log("run 7");
+  
+  this.play = function() { two.play(); }
+  this.pause = function() { two.pause(); }
+  this.regenerate = function() { this.grid = initializeGrid(this.grid); }
+  this.clear = function() { two.clear(); }
+  this.destroy = function() { this.clear(); null; };
+  
+  return this;
+
 }
 
-initializeGrid(grid);
-render(grid);
-
-two.bind("update", function(frameCount) {
-  if (!(frameCount % 1)) {
-    grid = stepGame(grid);
-    render(grid);
-    // console.log(frameCount);
-  }
-}).play();
-
-// two.update();
-
-console.log("done");
+// play game
+var game = new GameofLife(ngrid);
+game.play();
